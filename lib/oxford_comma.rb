07faf935d3ba4ccf34d5a49a08cb3fa0e3b2ec8c
Shilcof <robert.shilcof@gmail.com>
2020-10-20 17:52:03 +0100

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    array.collect{ |el|
+        if el == array[-1]
+            el
+        elsif el == array[-2] && array.size == 2
+            el = el + " and "
+        elsif el == array[-2]
+            el = el + ", and "
+        else
+            el = el + ", "
+        end
+    }.join
 end
